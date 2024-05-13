4577c98644e3f82189fa97ca3f064db02bc81397
mpatricio007 <mpatricio007@gmail.com>
2024-05-12 23:48:22 -0300

    Add USER_PASSWORD_AUTH

--- src/cognito.tf
+++ src/cognito.tf
@@ -34,15 +34,15 @@ resource "aws_cognito_user_pool_client" "fast_food_client" {
 
   # Configurações opcionais
   allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows = ["code", "implicit","user_password"]
+  allowed_oauth_flows = ["code", "implicit"]
   allowed_oauth_scopes = ["email", "openid", "profile"]
   callback_urls = ["https://example.com/callback"]
   logout_urls = ["https://example.com/logout"]
   supported_identity_providers = ["COGNITO"]
 
   # Configuração para desativar a verificação e confirmação assistida
-  #prevent_user_existence_errors = "ENABLED"
-  #explicit_auth_flows = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
+  prevent_user_existence_errors = "ENABLED"
+  explicit_auth_flows = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
 }
 
 output "user_pool_client_id" {
