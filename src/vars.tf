c646feeb875fc0b62455f5ea2218d90dd772ebe3
mpatricio007 <mpatricio007@gmail.com>
2024-05-13 20:22:15 -0300

    Update vars

--- src/vars.tf
+++ src/vars.tf
@@ -5,15 +5,3 @@ variable "regionDefault" {
 variable "projectName" {
   default = "fast-food"
 }
-
-variable "accessConfig" {
-  default = "API_AND_CONFIG_MAP"
-}
-
-variable "instanceType" {
-  default = "t3.small"
-}
-
-variable "policyArn" {
-  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-}
